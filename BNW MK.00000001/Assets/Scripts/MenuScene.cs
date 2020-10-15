79ed5f5c220698c1eb8f9f7ae3c05aa685bf398b
ElajahG <71793447+ElajahG@users.noreply.github.com>
2020-10-14 21:27:56 -0500

    menu

---
+++ BNW MK.00000001/Assets/Scripts/MenuScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScene : MonoBehaviour
+{
+    private CanvasGroup fadeGroup;
+    private float fadeInSpeed = 0.33f; // Speed the scene fades in
+
+    private void Start()
+    {
+        // Grab the only canvasGroup in the scene
+        fadeGroup = FindObjectOfType<CanvasGroup>();
+
+        // Start with a black screen
+        fadeGroup.alpha = 1;
+    }
+
+    private void Update()
+    {
+        fadeGroup.alpha = 1 - Time.timeSinceLevelLoad * fadeInSpeed;
+    }
+}
