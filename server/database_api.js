24f059e9ef612602e3e2c5d205eb17d5f173aa46
joshuacardoza99 <71305132+joshuacardoza99@users.noreply.github.com>
2020-10-16 15:47:42 -0500

    Update

--- server/database_api.js
+++ server/database_api.js
@@ -20,4 +20,65 @@ exports.database_api = function(serverName = "", username = "", password = "", d
    {
       console.log("Connected");
    })
+<<<<<<< Updated upstream
+=======
+   
+   // Private functions, string of arrays
+   var insert_data = function(tableName = "", tableColumns = [""], tableData = [""])
+   {
+      var sql = "INSERT INTO   " + tableName + "(" + tableColumns.toString() + 
+                ") VALUES (" + tableData.toString() + ");";
+  
+         connection.query(sql, (error) =>
+         {
+            if (error)
+            {
+               console.log("Insert Failed with the following error:" + error);
+            }
+         });
+   }
+
+   var select_data = function(tableName = "", receiver = (result) =>{})
+   {
+      var sql = "SELECT * FROM" + tableName + ";";
+
+         connection.query(sql, (error, result) =>
+         {
+            if (error)
+            {
+               console.log("Insert Failed with the following error:" + error);
+            }
+            else
+            {
+               receiver(result);
+            }
+         });
+   }
+
+   this.get_player = function (parameters)
+   {
+      var sql = "SELECT * FROM " + tableName + " ";
+
+      connection.query(sql, (error) =>
+      {
+         if (error)
+         {
+            console.log("Select statement failed with the following error:" + error);
+         }
+      });
+   }
+
+   // List of parameters starting with ip, username, match, civilization
+   this.add_player = function(parameters)
+   {
+      insert_data("players", ["ip, username, match, civilization"], parameters);
+   }
+
+   // List of parameters startingp
+   // Parameter = [GameObject, function]
+   this.get_player = function(receiver)
+   {
+      select_data("players", receiver);
+   }
+>>>>>>> Stashed changes
 }
