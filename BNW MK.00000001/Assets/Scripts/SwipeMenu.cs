2b28e31c46af584ecd983abf86f0fccf584365a0
lucasplos <lucasskywalker2@gmail.com>
2020-10-20 11:49:14 -0500

    update from backup

---
+++ BNW MK.00000001/Assets/Scripts/SwipeMenu.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwipeMenu : MonoBehaviour
+{
+    public GameObject scrollbar;
+    private float scroll_pos = 0;
+    float[] pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos = new float[transform.childCount];
+        float distance = 1f / (pos.Length - 1f);
+        for (int i = 0; i < pos.Length; i++)
+        {
+            pos[i] = distance * i;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            scroll_pos = scrollbar.GetComponent<Scrollbar>().value;
+        }
+        else
+        {
+            for (int i = 0; i < pos.Length; i++)
+            {
+                if (scroll_pos < pos[i] + (distance / 2) && scroll_pos > pos[i] - (distance / 2))
+                {
+                    scrollbar.GetComponent<Scrollbar>().value = Mathf.Lerp(scrollbar.GetComponent<Scrollbar>().value, pos[i], 0.1f);
+                }
+            }
+        }
+
+
+        for (int i = 0; i < pos.Length; i++)
+        {
+            if (scroll_pos < pos[i] + (distance / 2) && scroll_pos > pos[i] - (distance / 2))
+            {
+                Debug.LogWarning("Current Selected Level" + i);
+                transform.GetChild(i).localScale = Vector2.Lerp(transform.GetChild(i).localScale, new Vector2(1.2f, 1.2f), 0.1f);
+                for (int j = 0; j < pos.Length; j++)
+                {
+                    if (j != i)
+                    {
+                        transform.GetChild(j).localScale = Vector2.Lerp(transform.GetChild(j).localScale, new Vector2(0.8f, 0.8f), 0.1f);
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ BNW MK.00000001/Assets/Scripts/import_manager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class import_manager : MonoBehaviour
+{
+    // Global Variables //
+    server_manager serverManager;
+
+    // Unity Triggured Functions //
+
+    // Start is called before the first frame update (this is called by Unity).
+    // Style Exception: This function's name must be styled like this so Unity will run it.
+    void Start()
+    {
+        serverManager = GameObject.Find("network_manager").GetComponent<server_manager>();
+    }
+
+    // Public Funtions //
+
+    // Runs the given function on the local machine.
+    public void run_function(string gameObject, string function, string[] parameters)
+    {
+        try
+        {
+            GameObject.Find(gameObject).SendMessage(function, parameters);
+        }
+        catch
+        {
+            Debug.Log(gameObject + "." + function + " is not a local function.");
+        }
+    }
+
+    // Runs the given function on the local machine and all machines in the current match.
+    public void run_function_all(string gameObject, string function, string[] parameters)
+    {
+        run_function(gameObject, function, parameters);
+        serverManager.send(gameObject, function, parameters);
+    }
+}
