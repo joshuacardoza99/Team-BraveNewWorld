1b2d3cc2d35021fb974f1d28eb61fadef1f0c0d0
lucasplos <lucasskywalker2@gmail.com>
2020-10-20 20:14:48 -0500

    Revert "cleaning branch"
    This reverts commit 74fb77d089dca4f11f1efcc9d79e374e6b36e281.

---
+++ BNW MK.00000001/Assets/Scripts/PopUpPanel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpPanel : MonoBehaviour
+{
+    public GameObject Panel;
+
+    public void OpenPanel()
+    {
+        if (Panel != null)
+        {
+            Panel.SetActive(!Panel.activeSelf);
+        }
+    }
+}
+
---
+++ BNW MK.00000001/Assets/Scripts/match_manager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.WebCam;
+
+public class match_manager : MonoBehaviour
+{
+    // External Classes//
+    import_manager import_manager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        import_manager = GameObject.Find("network_manager").GetComponent<import_manager>();
+    }
+
+    // this is only for testing purposes before the menu UI is in place.
+    void Update()
+    {
+        // Easter egg to test the nextworking with.
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            start_network_game(new string[2] { "ElijahHero", "Asian" });
+
+            import_manager.run_function_all("server_functions", "get_player", new string[2] {"match_manager", "receive_test_data"});
+        }
+    }
+  
+    public void receive_test_data(string[] parameters)
+    {
+        Debug.Log(parameters[0]);
+    }
+    // Public Functions //
+
+    // Starts a multiplayer game over the network.
+    // parameters = [string name, string civilization]
+    public void start_network_game(string[] parameters)
+    {
+        import_manager.run_function_all("server_functions", "add_player", parameters);
+    }
+
+    // Starts a single player game locally.
+    // parameters = [string name, string civilization]
+    public void start_local_game(string[] parameters)
+    {
+        import_manager.run_function("Map", "load_map", parameters);
+    }
+
+    // Starts a multiplayer game over a private host.
+    // parameters = [string name, string civilization]
+    public void start_host_game(string[] parameters)
+    {
+
+    }
+
+    // Joins a multiplayer game over a private host.
+    // parameters = [string name, string civilization, string hostIP]
+    public void join_host_game(string[] parameters)
+    {
+
+    }
+}
