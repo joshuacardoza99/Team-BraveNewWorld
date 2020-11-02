2d78eaaac048d9faa3d64a18f89385a4fc860494
ElajahG <71793447+ElajahG@users.noreply.github.com>
2020-11-02 16:52:07 -0600

    menu update

---
+++ BNW MK.00000001/Assets/Scripts/menu_manager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu_manager : MonoBehaviour
+{
+    public GameObject[] groups;
+
+    public GameObject currentGroup;
+
+    void Awake()
+    {
+        ChangeGroup(currentGroup);
+    }
+
+    public void ChangeGroup(GameObject groupToActivate)
+    {
+
+        GameObject newGroup = groupToActivate;
+
+        foreach (GameObject group in groups)
+        {
+            if (group.name == newGroup.name)
+            {
+                group.SetActive(true);
+            }
+            else
+            {
+                group.SetActive(false);
+            }
+        }
+    }
+}
