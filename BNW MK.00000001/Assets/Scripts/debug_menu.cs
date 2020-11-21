a0d30b80f39ddfc47885fe4bde4f50cfd8ba7505
xenomorph321 <40804434+xenomorph321@users.noreply.github.com>
2020-11-21 10:49:33 -0600

    Adding Debug script

---
+++ BNW MK.00000001/Assets/Scripts/debug_menu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class debug_menu : MonoBehaviour
+{
+    UnityEvent debugMenu = new UnityEvent();
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Add a listener to the new Event. Calls MyAction method when invoked
+        debugMenu.AddListener(generateMenu);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public KeyDownEvent();
+    {
+        if (Input.GetKeyDown("p") && m_MyEvent != null)
+        {
+            Debug.Log("Menu");
+            debugMenu.RemoveListener(generateMenu);
+        }
+    }
