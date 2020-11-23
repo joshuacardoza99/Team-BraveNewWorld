6fd662879e4d01c8734c2d1c27b662a8628fc163
lucasplos <lucasskywalker2@gmail.com>
2020-11-23 15:23:37 -0600

    Adding city and building scripts for future use

---
+++ BNW MK.00000001/Assets/Scripts/Building.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// This script should be attached to all buildings
+public class Building : MonoBehaviour
+{
+    // External Classes//
+    import_manager import_manager;  // Import_Manager Class that facilitates cross class, player, and server function calls.
+
+    // Public Variables//
+    // variables concerning how this building affects the city (i.e. increased production, defense, etc.)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        import_manager = GameObject.Find("network_manager").GetComponent<import_manager>(); // Connects to the import_manager.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
