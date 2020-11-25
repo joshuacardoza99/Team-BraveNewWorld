df34924d946c2c65a768da4d581553db157ff91b
xenomorph321 <40804434+xenomorph321@users.noreply.github.com>
2020-11-25 15:10:38 -0600

    Prefabs

--- BNW MK.00000001/Assets/Scripts/debug_menu.cs
+++ BNW MK.00000001/Assets/Scripts/debug_menu.cs
@@ -1,28 +1,60 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour
+{
+    UnityEvent m_MyEvent;
+
+    void Start()
+    {
+        if (m_MyEvent == null)
+            m_MyEvent = new UnityEvent();
+
+        m_MyEvent.AddListener(Ping);
+        Debug.Log("Debug script is running!");
+    }
+
+    void Update()
+    {
+        if (Input.anyKeyDown && m_MyEvent != null)
+        {
+            m_MyEvent.Invoke();
+        }
+    }
+
+    void Ping()
+    {
+        Debug.Log("Ping");
+    }
+}
+
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class debug_menu : MonoBehaviour
 {
-    UnityEvent debugMenu = new UnityEvent();
+    UnityEvent m_MyEvent = new UnityEvent();
     // Start is called before the first frame update
     void Start()
     {
         //Add a listener to the new Event. Calls MyAction method when invoked
-        debugMenu.AddListener(generateMenu);
+        m_MyEvent.AddListener(generateMenu);
     }
 
     // Update is called once per frame
     void Update()
     {
-
-    }
-
-    public KeyDownEvent();
-    {
-        if (Input.GetKeyDown("p") && m_MyEvent != null)
+        if (Input.GetKeyDown(KeyCode.P) && m_MyEvent != null)
         {
-            Debug.Log("Menu");
-            debugMenu.RemoveListener(generateMenu);
+            m_MyEvent.Invoke();
         }
     }
+
+    public void generateMenu()
+    {
+        Debug.Log("Menu generated");
+    }
+}*/
